97fbbd2ea44b1aba8c5ebae66828b27707267094
Alexander <xapip@mail.ru>
2025-07-08 22:02:41 +0300

    install eruda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { initDataState as _initDataState, useSignal } from '@telegram-apps/sdk-react'
+import eruda from "eruda";
 
 export default function Home() {
+  eruda.init()
+
   const telegramData = useSignal(_initDataState)
   
   return (
