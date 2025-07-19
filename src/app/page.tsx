32a9493e5bb88b7e4f858a5258e9a2c7ca115b37
Alexander <xapip@mail.ru>
2025-07-19 14:41:13 +0300

    add prettier and husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-'use client'
+"use client"
 
-import { initDataState as _initDataState, useSignal } from '@telegram-apps/sdk-react'
-import eruda from "eruda";
+import React from "react"
+import {
+    initDataState as _initDataState,
+    useSignal,
+} from "@telegram-apps/sdk-react"
+import eruda from "eruda"
 
 export default function Home() {
-  eruda.init()
+    eruda.init()
 
-  const telegramData = useSignal(_initDataState)
-  
-  return (
-    <>
-      <h1>{telegramData?.user?.first_name}</h1>
-    </>
-  )
+    const telegramData = useSignal(_initDataState)
+
+    return (
+        <>
+            <h1>{telegramData?.user?.first_name}</h1>
+        </>
+    )
 }
